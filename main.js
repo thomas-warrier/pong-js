2c34e634c64997ed1cfefb51e7d0f5e4385fe418
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-14 16:54:08 +0100

    ajout des fonctions pour les événements dans main.js et ajout des imports au html

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const height = can.height;
 const FPS = 1 / 60;
 ctx = can.getContext("2d");
 
+
+
+function pressKey(ev) {
+}
+function releaseKey(ev) {
+}
+function update() {
+    draw();
+}
+function draw() {
+    clear();
+}
+
+setInterval(update, FPS * 1000);
+document.addEventListener("keydown", pressKey);
+document.addEventListener("keyup", releaseKey);
+
