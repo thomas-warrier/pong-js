389fa7eb4690c699f303bc56f8d6016589cd98e4
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-11-29 12:57:44 +0100

    ajout du menu draw win

---
+++ drawWin.js
@@ -0,0 +1,6 @@
+function drawWin(player){
+    drawText(`Bien joué ${player} vous avez gagné !`,width/2,height/3);
+    new Button("Recommencer");
+    new Button("Menu Principale");
+    
+}
--- globalVariable.js
+++ globalVariable.js
@@ -5,7 +5,6 @@ const FPS = 1 / 60; //pour avoir 60 images par seconde
 ctx = can.getContext("2d"); //récupère le context 2d du canva 
 var myFont = new FontFace('Audiowide', 'url(assets/AudiowideRegular.ttf)'); //crée une nouvelle font face
 myFont.load().then(function(font){
-    
     document.fonts.add(font);
     ctx.font = "50px Audiowide"; //je set la font
     ctx.textAlign = "center";
